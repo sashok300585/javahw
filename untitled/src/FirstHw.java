94b9c166c3915128f77fc63f884a5d0efc0889d5
sashok300585 <titovichalex@gmail.com>
2022-04-21 18:01:17 +0300

    javahw

--- untitled/src/FirstHw.java
+++ untitled/src/FirstHw.java
@@ -1,5 +1,6 @@
 public class FirstHw {
     public static void main(String[] args) {
         System.out.println("Hello my friends");
+        System.out.println("Will it work or won't it");
     }
 }
